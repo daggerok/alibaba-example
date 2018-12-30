7a32cc50f61f023e36cb8a50f5dc268b1a5afdc5
Maksim Kostromin <daggerok@gmail.com>
2018-12-30 03:35:11 +0200

    Implement basic sentinel rule.

--- my-app/src/main/java/com/github/daggerok/alibaba/app/MyApp.java
+++ my-app/src/main/java/com/github/daggerok/alibaba/app/MyApp.java
@@ -1,11 +1,44 @@
 package com.github.daggerok.alibaba.app;
 
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class MyApp {
   public static void main(String[] args) {
     SpringApplication.run(MyApp.class);
+  }
+}
+
+@RestController
+class HelloResource {
+  @GetMapping("/hey")
+  public ResponseEntity hey() {
+    return ResponseEntity.ok("hey!");
+  }
+}
+
+@Configuration
+class HeyConfig {
+
+  @EventListener(ApplicationStartedEvent.class)
+  public void secureHey() {
+    final FlowRule rule = new FlowRule()
+        .setGrade(RuleConstant.FLOW_GRADE_QPS)
+        .setCount(2);
+    rule.setResource("/hey")
+        .setLimitApp(RuleConstant.LIMIT_APP_DEFAULT);
+    FlowRuleManager.loadRules(Collections.singletonList(rule));
   }
 }
